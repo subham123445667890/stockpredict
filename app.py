4f2f84f061ce3a226b0ec66690f3d0986155a385
Subham <126056594+subham123445667890@users.noreply.github.com>
2023-12-05 15:32:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
+import requests
+import lxml
 from keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
