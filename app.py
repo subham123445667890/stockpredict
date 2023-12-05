c0ea4822c4033005db01eb5732e92abe18a6407b
Subham <126056594+subham123445667890@users.noreply.github.com>
2023-12-05 15:04:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
+pip install yfinance
+venv\Scripts\activate
 from keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
